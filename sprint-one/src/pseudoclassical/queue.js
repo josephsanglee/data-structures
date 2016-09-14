d07073a9a9d03797a6398b03802b70e2d0b6f61a
josephsanglee <josephsanglee@gmail.com>
2016-09-14 16:28:20 -0700

    Implement the pseudoclassical pattern for quene, stack

--- sprint-one/src/pseudoclassical/queue.js
+++ sprint-one/src/pseudoclassical/queue.js
@@ -1,6 +1,35 @@
-var Queue = function() {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
+var Queue = function(counter, storage) {
+  this.counter = counter || 0;
+  this.storage = storage || {};
 };
+
+Queue.prototype.enqueue = function(value) {
+  this.storage[this.counter] = value;
+  this.counter++;
+};
+
+Queue.prototype.dequeue = function() {
+  var result = this.storage[0];
+  if (this.counter > 0) {
+    this.counter--; 
+  }
+  
+  delete this.storage[0];
+  
+  var line = Object.keys(this.storage);
+  
+  for (var i = 0; i < line.length; i++) {
+    this.storage[i] = this.storage[i + 1];
+  }
+  
+  delete this.storage[line.length];
+  
+  return result;
+};
+
+Queue.prototype.size = function() {
+  return this.counter;
+};
+
 
 
