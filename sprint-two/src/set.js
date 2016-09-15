593ec7c7e26c0eb13e1b95798196a79eec31964e
josephsanglee <josephsanglee@gmail.com>
2016-09-15 14:44:31 -0700

    Implement set and pass all tests

--- sprint-two/src/set.js
+++ sprint-two/src/set.js
@@ -1,20 +1,30 @@
 var Set = function() {
   var set = Object.create(setPrototype);
-  set._storage = undefined;
+  set._storage = {};
   return set;
 };
 
 var setPrototype = {};
 
 setPrototype.add = function(item) {
+  this._storage[item] = item;
 };
 
 setPrototype.contains = function(item) {
+  for (var key in this._storage) {
+    if (key === item) {
+      return true;
+    }
+  }
+  return false;
 };
 
 setPrototype.remove = function(item) {
+  delete this._storage[item];
 };
 
 /*
  * Complexity: What is the time complexity of the above functions?
+   adds/delete = O(1)
+   gets = O(n)
  */
