e4618dd455ba91c9f0d254a720e4fc256ada918c
josephsanglee <josephsanglee@gmail.com>
2016-09-15 09:32:13 -0700

    Implement linked-list and pass all tests

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -4,12 +4,38 @@ var LinkedList = function() {
   list.tail = null;
 
   list.addToTail = function(value) {
+    var node = Node(value);
+
+    if (list.head === null) {
+      list.head = node;
+      list.tail = node;
+    } else if (list.head.next === null) {
+      list.head.next = node;
+      list.tail = node;
+    } else {
+      var currentTail = list.tail;
+      list.tail = node;
+      currentTail.next = list.tail;
+    }
+
   };
 
   list.removeHead = function() {
+    var nextHead = list.head.next;
+    var removeHeadValue = list.head.value;
+    delete list.head;
+
+    list.head = nextHead;
+    return removeHeadValue;
   };
 
   list.contains = function(target) {
+    for (var key in list) {
+      if (list[key].value === target) {
+        return true;
+      }
+    } 
+    return false;
   };
 
   return list;
