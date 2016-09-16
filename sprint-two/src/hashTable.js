339be1ddf604eeca988f8006a0b631f4b089592a
josephsanglee <josephsanglee@gmail.com>
2016-09-15 20:19:45 -0700

    Implement hash table and pass all tests

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -7,20 +7,38 @@ var HashTable = function() {
 
 HashTable.prototype.insert = function(k, v) {
   var index = getIndexBelowMaxForKey(k, this._limit);
-
-  this._storage.set(index, v);  
+  if (this._storage.get(index) === undefined) {
+    this._storage.set(index, [[k, v]]);
+  } else {
+    for (var i = 0; i < this._storage.get(index).length; i++) {
+      if (this._storage.get(index)[i][0] === k ) {
+        this._storage.get(index)[i][1] = v;
+      }
+    }
+  }
+  this._storage.get(index).push([k, v]);
 };
 
 HashTable.prototype.retrieve = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
 
-  return this._storage.get(index);
+  for (var i = 0; i < this._storage.get(index).length; i++) {
+    if (this._storage.get(index)[i][0] === k) {
+      return this._storage.get(index)[i][1];
+    }
+  }
+  return undefined;
 };
 
 HashTable.prototype.remove = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
 
-  this._storage.set(index, undefined);
+  for (var i = 0; i < this._storage.get(index).length; i++) {
+    if (this._storage.get(index)[i][0] === k) {
+      //delete this._storage.get(index)[i][0];
+      this._storage.get(index)[i].splice(1, 1);
+    }
+  }
 };
 
 
