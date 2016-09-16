f252c9a0144b0c6b6fbb1a8262f78c02a4a111ba
josephsanglee <josephsanglee@gmail.com>
2016-09-15 20:33:02 -0700

    Set up bst

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -1,4 +1,22 @@
 var BinarySearchTree = function(value) {
+  var newTree = Object.create(treeMethods);
+  var left = null;
+  var right = null;
+
+
+  return newTree;
+};
+
+var treeMethods = {
+  insert: function(value) {
+
+  },
+  contains: function(value) {
+
+  },
+  depthFirstLog: function(callback) {
+
+  }
 };
 
 
